6d4b68d04cfda3c8f67bf382435d50b5f48536de
Md Ashad <asadanwarr0@gmail.com>
2025-11-07 02:12:30 +0530

    Update TodoList.jsx

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -105,7 +105,7 @@ export default function TodoList() {
                             </button>
                         </div>
                     </li>
-                ))}
+                ))};
             </ul>
 
             {todos.length > 0 && (
