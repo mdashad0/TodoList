bb5e701e003f015e90f314d89b6bc7f0714771da
Md Ashad <asadanwarr0@gmail.com>
2025-11-07 02:13:01 +0530

    Update TodoList.jsx

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -115,7 +115,7 @@ export default function TodoList() {
                 >
                     Uppercase All Tasks
                 </button>
-            )}
+            )};
         </div>
     );
 };
